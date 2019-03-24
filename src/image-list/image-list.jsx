39ed10c2c2e869065cf4d802fbb522b428965637
pnolan89 <petenol@gmail.com>
2019-03-24 19:30:29 +0000

    add image component

--- src/image-list/image-list.jsx
+++ src/image-list/image-list.jsx
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
 import './image-list.css';
+import Image from './image';
 
 class ImageList extends Component {
   constructor(props) {
     super(props);
   }
 
-
-
   render() {
+    const gifdata = [1, 2, 3, 4, 5];
+    const images = gifdata.map((image) =>
+      <Image image={image}/>
+    )
     return (
-      <h1>I AM IMAGE LIST! FEAR ME MORTALS!</h1>
+      <main className="images">
+        {images}
+      </main>
     );
   }
 }
