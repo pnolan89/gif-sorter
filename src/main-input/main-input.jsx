c18e6bee20449b9855c4596991d7df3cb56a1fde
pnolan89 <petenol@gmail.com>
2019-03-24 17:53:37 +0000

    main input text updates state

--- src/main-input/main-input.jsx
+++ src/main-input/main-input.jsx
@@ -2,8 +2,27 @@ import React, { Component } from "react";
 import './main-input.css';
 
 class MainInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mainInput: ""
+    };
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
   render() {
-    return <h1>I am Main Input</h1>
+    return (
+      <form id="main-input">
+        <input id="main-input-textbox" name="mainInput" type="text" onChange={this.onChange}/>
+
+      </form>
+    );
   }
 }
 
