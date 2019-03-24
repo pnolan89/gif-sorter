9a7de69e6a440c05b74b6d4671db3c03fbca95b8
pnolan89 <petenol@gmail.com>
2019-03-24 17:37:48 +0000

    initial commit

---
+++ src/main-input/main-input.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import './main-input.css';
+
+class MainInput extends Component {
+  render() {
+    return <h1>I am Main Input</h1>
+  }
+}
+
+export default MainInput;
